32ca49f439a2f8509187797205e1c435b9004a5f
Dan Thompson <dthompson@fitbit.com>
2020-05-26 09:22:13 -0400

    Adding in longer waits

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,16 @@ fn main() {
     let five_secs = time::Duration::from_secs(5);
     let one_sec = time::Duration::from_millis(400);
     thread::sleep(five_secs);
-    for i in 0..243 {
+    for i in 0..1912 {
       println!("Starting");
       let mut rng = thread_rng();
       let n: u64 = rng.gen_range(0, 1000);
+      let wait_chance: u64 = rng.gen_range(0, 300);
       let fuzz = time::Duration::from_millis(n);
+      if wait_chance > 295 {
+        println!("We in the big wait now");
+        thread::sleep(five_secs + fuzz*5);
+      }
       println!("Fuzz: {:?}", fuzz);
       thread::sleep(one_sec + fuzz);
       println!("clicking!");
